ae3bc3f57e8d00b30c1c2d8aa851189364578538
Guims04 <gem.epifanio@gmail.com>
2023-09-02 14:27:28 -0300

    ajustando funcionalidade da execução da operação

--- src/perform_operation/Operation.java
+++ src/perform_operation/Operation.java
@@ -9,9 +9,11 @@ import java.util.regex.Pattern;
 
 public class Operation extends Connectives {
 	public static Map<String, boolean[]> charArrayMap = new HashMap<>();
+
 	public Operation(int numberOfPrepositions) {
 		super(numberOfPrepositions);
 	}
+
 	public static final Character[] SimbolosPermitidos = {
 			'^', 'v', '→', '↔', '~'
 	};
@@ -19,15 +21,17 @@ public class Operation extends Connectives {
 			'(', ')'
 	};
 
+	private static int formulaProgression = 0;
+
 	public enum TypeOfOperation {
 		isConnective,
 		isConjunction,
 		isDisjunction,
 		isConditional,
-		isDenial,
 		isBiconditional;
 	}
 
+	// Check if the operations only contains the especified propositions
 	public static boolean ProposiçõesValidas(String formula, ArrayList<Character> arraylist) {
 		for (char i : formula.toCharArray()) {
 			char iUpper = Character.toUpperCase(i);
@@ -38,6 +42,7 @@ public class Operation extends Connectives {
 		return true;
 	}
 
+	// validate the formula
 	public static boolean SimbolosValidos(String formula) {
 		Stack<Character> stack = new Stack<>();
 		boolean isConnective = false;
@@ -82,6 +87,8 @@ public class Operation extends Connectives {
 		return stack.isEmpty();
 	}
 
+	// Aids in the SimbolosValidos function to check whether a specific symbol
+	// according to the characters array passed to it.
 	private static boolean isSimboloPermitido(char c, Character[] simbols) {
 		for (char permitido : simbols) {
 			if (c == permitido) {
@@ -91,6 +98,7 @@ public class Operation extends Connectives {
 		return false;
 	}
 
+	// return if array is valid or not
 	public static boolean validator(String formula, ArrayList<Character> arraylist) {
 		if (SimbolosValidos(formula) && ProposiçõesValidas(formula, arraylist)) {
 			System.out.println("É válido");
@@ -101,6 +109,7 @@ public class Operation extends Connectives {
 		}
 	}
 
+	// create the proposition's array
 	public static void creatPropArrays(ArrayList<Character> preosition) {
 		for (int i = numberOfPrepositions - 1; i >= 0; i--) {
 			boolean[] currentColumn = new boolean[numberInColumn];
@@ -119,54 +128,107 @@ public class Operation extends Connectives {
 		}
 	}
 
-	public static void prepareToOperate(String formula) {
-		String aux = "";
-		String index;
+	// go through the string formula preparing and operating then
+	public static String prepareToOperate(String formula) {
+		String prev = "";
+		String actual = "";
+		String index = "";
+		String lastResult = "";
+		boolean denial = false;
+		// boolean canOneByOne = true;
 
 		TypeOfOperation currentOperation = TypeOfOperation.isConnective;
 
-		for (char f : formula.toCharArray()) {
-			if (currentOperation == TypeOfOperation.isConjunction) {
-				System.out.println(aux);
-				boolean[] result = Connectives.conjunction(charArrayMap.get(aux), charArrayMap.get(f + ""));
-				index = aux + "^" + f;
+		char[] form = formula.toCharArray();
+
+		System.out.println("formula inicio:" + formula);
+
+		for (int i = 0; i < form.length; i++) {
+			System.out.println(currentOperation);
+			char f = form[i];
+			formulaProgression++;
+
+			if (f == '(') {
+				System.out.println("formula progression: " + formulaProgression);
+				String newFormula = removeCharacter(formula, formulaProgression);
+				String aux = prepareToOperate(newFormula);
+				System.out.println("aux: " + aux);
+				System.out.println("index2: " + index);
+				if (currentOperation != TypeOfOperation.isConnective) {
+					actual = aux;
+					if (index != "")
+						prev = index;
+				} else
+					prev = aux;
+				i = formulaProgression - 1;
+				f = form[i];
+				System.out.println("new f: " + f);
+			} else if (f == ')') {
+				lastResult = index;
+				index = "";
+				return lastResult;
+			} else {
+				actual = f + "";
+			}
+
+			// System.out.println(f);
+
+			if (denial) {
+				boolean[] result = Connectives.denial(charArrayMap.get(actual));
+				index = "~" + actual;
+				charArrayMap.put(index, result);
+				actual = index;
+				denial = false;
+				System.out.println("Actual: " + actual);
+			}
+
+			if (f == '~')
+				denial = true;
+
+			if (currentOperation == TypeOfOperation.isConjunction && !denial) {
+				boolean[] result = Connectives.conjunction(charArrayMap.get(prev), charArrayMap.get(actual));
+				index = "(" + prev + "^" + actual + ")";
+				System.out.println("index: " + index);
 				charArrayMap.put(index, result);
 			}
 
-			if (currentOperation == TypeOfOperation.isDisjunction) {
-				boolean[] result = Connectives.disjunction(charArrayMap.get(aux), charArrayMap.get(f + ""));
-				index = aux + "v" + f;
+			if (currentOperation == TypeOfOperation.isDisjunction && !denial) {
+				System.out.println(prev + "v" + actual);
+				boolean[] result = Connectives.disjunction(charArrayMap.get(prev), charArrayMap.get(actual));
+				index = "(" + prev + "v" + actual + ")";
+				System.out.println("index: " + index);
 				charArrayMap.put(index, result);
 			}
 
-			if (currentOperation == TypeOfOperation.isConditional) {
-				boolean[] result = Connectives.conditional(charArrayMap.get(aux), charArrayMap.get(f + ""));
-				index = aux + "→" + f;
+			if (currentOperation == TypeOfOperation.isConditional && !denial) {
+				boolean[] result = Connectives.conditional(charArrayMap.get(prev), charArrayMap.get(actual));
+				index = "(" + prev + "→" + actual + ")";
+				System.out.println("index: " + index);
 				charArrayMap.put(index, result);
 			}
 
-			if (currentOperation == TypeOfOperation.isBiconditional) {
-				boolean[] result = Connectives.biconditional(charArrayMap.get(aux), charArrayMap.get(f + ""));
-				index = aux + "↔" + f;
+			if (currentOperation == TypeOfOperation.isBiconditional && !denial) {
+				boolean[] result = Connectives.biconditional(charArrayMap.get(prev), charArrayMap.get(actual));
+				index = "(" + prev + "↔" + actual + ")";
+				System.out.println("index: " + index);
 				charArrayMap.put(index, result);
 			}
 
-			if (currentOperation == TypeOfOperation.isDenial) {
-				boolean[] result = Connectives.denial(charArrayMap.get(f + ""));
-				index = "~"+f;
-				charArrayMap.put(index, result);
+			if (!denial)
+				currentOperation = checkKindOfOperation(f);
+
+			if (Character.isLetter(f) && f != 'V' && index == "") {
+				prev = f + "";
+			} else if (Character.isLetter(f) && f != 'V') {
+				prev = index;
 			}
 
-			currentOperation = checkKindOfOperation(f);
-
-			if (Character.isLetter(f) && f != 'V') {
-				aux = f + "";
-			}
 		}
-		tableAppearance();
+		return "";
 	}
 
-	public static TypeOfOperation checkKindOfOperation (char character) {
+	// Aids prepareToOperate by checking wictch operation has to do.
+	public static TypeOfOperation checkKindOfOperation(char character) {
 		switch (character) {
 			case '^':
 				return TypeOfOperation.isConjunction;
@@ -176,13 +238,12 @@ public class Operation extends Connectives {
 				return TypeOfOperation.isConditional;
 			case '↔':
 				return TypeOfOperation.isBiconditional;
-			case '~':
-				return TypeOfOperation.isDenial;
 		}
 		return TypeOfOperation.isConnective;
 	}
 
-	public static void tableAppearance () {
+	// display the table result
+	public static void tableAppearance() {
 		charArrayMap.forEach((key, value) -> {
 			System.out.print(key + " | ");
 		});
@@ -204,6 +265,18 @@ public class Operation extends Connectives {
 		while (matcher.find()) {
 			String expression = matcher.group(1);
 			System.out.println(expression);
+		}
+	}
+
+	// Aid prepareToOperate removing the formula caracteres to operate without then.
+	public static String removeCharacter(String input, int quantidade) {
+		// Verificar se a quantidade é válida
+		if (quantidade >= 0 && quantidade <= input.length()) {
+			// Remover os caracteres da esquerda para a direita
+			return input.substring(quantidade);
+		} else {
+			// Se a quantidade não for válida, retornar a string original
+			return input;
 		}
 	}
 
